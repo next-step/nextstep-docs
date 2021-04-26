a293eca4b514649c1e72b27bc04899a0c61bb526
knecht <ldj910505@naver.com>
2021-04-26 23:20:41 +0900

    refactor(전역) : 10라인 이하로 맞췄습니다.
    요구사항이 숨어 있네요;;;
    10라인 이하로 맞췄습니다.
    
    감사합니다.

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,6 @@ public class Main {
 		sc.close();
 	}
 
-	/**
-	 * 게임 플레이
-	 * 
-	 * @param answer
-	 * @param flag
-	 */
 	private static void gamePlay(String answer, int flag) {
 		while (flag != 2) {
 			answer = generateAnswer(new String(answer));
@@ -31,105 +25,116 @@ public class Main {
 		}
 	}
 
+	private static void guide() {
+		System.out.print("숫자를 입력해주세요 : ");
+	}
+
+	private static int guide(int[] input) {
+		String line = mentMaking(input);
+		System.out.println(line);
+		if (input[0] == 3) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			int flag = sc.nextInt();
+			return flag;
+		}
+		return 0;
+	}
+
+	private static String mentMaking(int[] input) {
+		String ret = "";
+		ret = mentMakingStrike(input, ret);
+		ret = mentMakingBall(input, ret);
+		ret = mentMakingNothing(input, ret);
+		return ret;
+	}
+
+	private static String mentMakingStrike(int[] input, String ret) {
+		if (input[0] > 0) {
+			ret += input[0] + " 스트라이크 ";
+		}
+		return ret;
+	}
+	
+	private static String mentMakingBall(int[] input, String ret) {
+		if (input[1] > 0) {
+			ret += input[1] + " 볼";
+		}
+		return ret;
+	}
+
+	private static String mentMakingNothing(int[] input, String ret) {
+		if (input[0] == 0 && input[1] == 0) {
+			ret += "낫싱";
+		}
+		return ret;
+	}
+
+	private static int[] calculate(String input, String answer) {
+		int[] ret = new int[2];
+		ret[0] = calculateStrike(input, answer);
+		ret[1] = calculateBall(input, answer);
+		return ret;
+	}
+
+	private static int calculateBall(String input, String answer) {
+		int ret = 0;
+		ret = calculateMatch(input.charAt(0), answer.charAt(1), ret);
+		ret = calculateMatch(input.charAt(0), answer.charAt(2), ret);
+		ret = calculateMatch(input.charAt(1), answer.charAt(0), ret);
+		ret = calculateMatch(input.charAt(1), answer.charAt(2), ret);
+		ret = calculateMatch(input.charAt(2), answer.charAt(0), ret);
+		ret = calculateMatch(input.charAt(2), answer.charAt(1), ret);
+		return ret;
+	}
+
+	private static int calculateStrike(String input, String answer) {
+		int ret = 0;
+		ret = calculateMatch(input.charAt(0), answer.charAt(0), ret);
+		ret = calculateMatch(input.charAt(1), answer.charAt(1), ret);
+		ret = calculateMatch(input.charAt(2), answer.charAt(2), ret);
+		return ret;
+	}
+
+	private static int calculateMatch(char a, char b, int ret) {
+		if (a == b) {
+			return ret + 1;
+		}
+		return ret;
+	}
+
+	private static String generateAnswer(String answer) {
+		if (isSafe(answer)) {
+			return answer;
+		}
+		return generateNewAnswer();
+	}
+
+	private static boolean isSafe(String answer) {
+		if (answer.charAt(0) == answer.charAt(1) || answer.charAt(1) == answer.charAt(2) || answer.charAt(0) == answer.charAt(2)) {
+			return false;
+		}
+		if (answer.charAt(0) == '0' || answer.charAt(1) == '0' || answer.charAt(2) == '0') {
+			return false;
+		}
+		return true;
+	}
+
+	private static String generateNewAnswer() {
+		String answer = "";
+		int temp = (int) (Math.random() * 999 + 1);
+		answer = temp + "";
+		if (temp >= 100 && temp <= 999 && isSafe(answer)) {
+			return answer;
+		}
+		return generateNewAnswer();
+	}
+
 	private static String initAnswer(int flag, String answer) {
 		if (flag == 0 || flag == 2) {
 			return answer;
 		}
 		if (flag == 1) {
 			return "111";
-		}
-		return answer;
-	}
-
-	private static int guide(int[] ret) {
-
-		String line = "";
-		if (ret[0] > 0) {
-			line += ret[0] + " 스트라이크 ";
-		}
-		if (ret[1] > 0) {
-			line += ret[1] + " 볼";
-		}
-		if(ret[0] == 0 && ret[1] == 0) {
-			line += "낫싱";
-		}
-		System.out.println(line);
-
-		if (ret[0] == 3) {
-			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-			int flag = sc.nextInt();
-			return flag;
-		}
-		// 쓰리 스트라이크가 아니면 게임을 계속 해야함
-		return 0;
-
-	}
-
-	/**
-	 * @param input
-	 *            내가 입력한 숫자
-	 * @param answer
-	 *            정답
-	 * @return 스트라이크, 볼 수
-	 */
-	private static int[] calculate(String input, String answer) {
-
-		int[] ret = new int[2];
-		if (input.charAt(0) == answer.charAt(0)) {
-			ret[0]++;
-		}
-		if (input.charAt(0) == answer.charAt(1)) {
-			ret[1]++;
-		}
-		if (input.charAt(0) == answer.charAt(2)) {
-			ret[1]++;
-		}
-		if (input.charAt(1) == answer.charAt(0)) {
-			ret[1]++;
-		}
-		if (input.charAt(1) == answer.charAt(1)) {
-			ret[0]++;
-		}
-		if (input.charAt(1) == answer.charAt(2)) {
-			ret[1]++;
-		}
-		if (input.charAt(2) == answer.charAt(0)) {
-			ret[1]++;
-		}
-		if (input.charAt(2) == answer.charAt(1)) {
-			ret[1]++;
-		}
-		if (input.charAt(2) == answer.charAt(2)) {
-			ret[0]++;
-		}
-		return ret;
-	}
-
-	private static void guide() {
-		System.out.print("숫자를 입력해주세요 : ");
-	}
-
-	/*
-	 * 입력이 정상이면 정상을 그대로 리턴
-	 * 입력이 정상이 아니어도 정상을 리턴
-	 */
-	private static String generateAnswer(String answer) {
-		if (answer.length() == 3 && answer.charAt(0) != answer.charAt(1) && answer.charAt(1) != answer.charAt(2) && answer.charAt(0) != answer.charAt(2)) {
-			return answer;
-		}
-		while (true) {
-			int temp = (int) (Math.random() * 999 + 1);
-			if (temp < 100 || temp >= 1000) {
-				continue;
-			}
-			answer = temp + "";
-			if (answer.charAt(0) == '0' || answer.charAt(1) == '1' || answer.charAt(2) == 2) {
-				continue;
-			}
-			if (answer.length() == 3 && answer.charAt(0) != answer.charAt(1) && answer.charAt(1) != answer.charAt(2) && answer.charAt(0) != answer.charAt(2)) {
-				break;
-			}
 		}
 		return answer;
 	}
