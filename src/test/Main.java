cd2c3bad98380fb81f135559a4a9e47f97581e7a
knecht <ldj910505@naver.com>
2021-04-26 23:41:31 +0900

    <test> : 처음 테스트 파일을 만들어 넣었습니다.
    테스트 파일을 처음 만들어 넣었습니다.

---
+++ src/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Main {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
