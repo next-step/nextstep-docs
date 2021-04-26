9a1f817aec518df8f217326bd1d724f898284cc4
knecht <ldj910505@naver.com>
2021-04-26 20:55:54 +0900

    가이드까지 끝냄

--- src/Main.java
+++ src/Main.java
@@ -15,34 +15,82 @@ public class Main {
 			System.out.println();
 			int[] ret = calculate(input, answer);
 			flag = guide(ret);
-			guide(flag);
+			guide(flag, answer);
 		}
-
-		A: while (true) {
-			String answer = "111";
-
-			
-
-			System.out.println("숫자를 입력해주세요 : ");
-			String line = sc.next();
-
-			B: while (answer.equals(line)) {
-				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-				System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-				String command = sc.next();
-				if ("1".equals(command)) {
-					break B;
-				} else if ("2".equals(command)) {
-					break A;
-				} else {
-					System.out.println("다시 입력해주세요.");
-				}
-			}
-
-		}
-
 		sc.close();
 
+	}
+
+
+
+	private static void guide(boolean flag, String answer) {
+		if(flag) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			return;
+		}
+	}
+
+
+
+	private static boolean guide(int[] ret) {
+		
+		String line = "";
+		if(ret[0] > 0) {
+			line += ret[0] + " 스트라이크";
+		}
+		if(ret[1] > 0) {
+			line += ret[1] + " 볼";
+		}
+		System.out.println(line);
+		
+		if(ret[0] == 3) {
+			return true;
+		}else {
+			return false;			
+		}
+		
+		
+	}
+
+
+
+	/**
+	 * @param input 내가 입력한 숫자
+	 * @param answer 정답
+	 * @return 스트라이크, 볼 수
+	 */
+	private static int[] calculate(String input, String answer) {
+	
+		int [] ret = new int[2];
+		if(input.charAt(0) == answer.charAt(0)) {
+			ret[0]++;
+		}
+		if(input.charAt(0) == answer.charAt(1)) {
+			ret[1]++;
+		}
+		if(input.charAt(0)== answer.charAt(2)) {
+			ret[1]++;
+		}
+		if(input.charAt(1) == answer.charAt(0)) {
+			ret[1]++;
+		}
+		if(input.charAt(1) == answer.charAt(1)) {
+			ret[0]++;
+		}
+		if(input.charAt(1)== answer.charAt(2)) {
+			ret[1]++;
+		}
+		if(input.charAt(2) == answer.charAt(0)) {
+			ret[1]++;
+		}
+		if(input.charAt(2) == answer.charAt(1)) {
+			ret[1]++;
+		}
+		if(input.charAt(2)== answer.charAt(2)) {
+			ret[0]++;
+		}
+		return ret;
 	}
 
 
