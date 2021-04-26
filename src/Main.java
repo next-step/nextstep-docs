4c88961bb4294060da96e9dc62a6ccd93e51e8d0
knecht <ldj910505@naver.com>
2021-04-26 21:45:27 +0900

    종료하려면 2를 입력하는 것도 구현

--- src/Main.java
+++ src/Main.java
@@ -2,104 +2,106 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	private static Scanner sc = new Scanner(System.in);
 
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
 		String answer = "111";
 		boolean flag = true;
 		while (flag) {
 			String temp = new String(answer);
-			answer = generate(temp);
+			answer = generateAnswer(temp);
 			guide();
 			String input = sc.next();
-			System.out.println();
 			int[] ret = calculate(input, answer);
 			flag = guide(ret);
-			guide(flag, answer);
+			answer = initAnswer(flag, answer);
 		}
 		sc.close();
-
 	}
 
-
-
-	private static void guide(boolean flag, String answer) {
-		if(flag) {
-			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-			return;
+	private static String initAnswer(boolean flag, String answer) {
+		if (flag == false) {
+			return "111";
+		} else {
+			return answer;
 		}
 	}
-
-
 
 	private static boolean guide(int[] ret) {
-		
+
 		String line = "";
-		if(ret[0] > 0) {
+		if (ret[0] > 0) {
 			line += ret[0] + " 스트라이크";
 		}
-		if(ret[1] > 0) {
+		if (ret[1] > 0) {
 			line += ret[1] + " 볼";
 		}
 		System.out.println(line);
-		
-		if(ret[0] == 3) {
+
+		if (ret[0] == 3) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			int flag = sc.nextInt();
+			return playNext(flag);
+		} else {
 			return true;
-		}else {
-			return false;			
 		}
-		
-		
+
 	}
 
-
+	private static boolean playNext(int flag) {
+		if(flag == 1) {
+			return false;
+		}else {
+			return true;			
+		}
+	}
 
 	/**
-	 * @param input 내가 입력한 숫자
-	 * @param answer 정답
+	 * @param input
+	 *            내가 입력한 숫자
+	 * @param answer
+	 *            정답
 	 * @return 스트라이크, 볼 수
 	 */
 	private static int[] calculate(String input, String answer) {
-	
-		int [] ret = new int[2];
-		if(input.charAt(0) == answer.charAt(0)) {
+
+		int[] ret = new int[2];
+		if (input.charAt(0) == answer.charAt(0)) {
 			ret[0]++;
 		}
-		if(input.charAt(0) == answer.charAt(1)) {
+		if (input.charAt(0) == answer.charAt(1)) {
 			ret[1]++;
 		}
-		if(input.charAt(0)== answer.charAt(2)) {
+		if (input.charAt(0) == answer.charAt(2)) {
 			ret[1]++;
 		}
-		if(input.charAt(1) == answer.charAt(0)) {
+		if (input.charAt(1) == answer.charAt(0)) {
 			ret[1]++;
 		}
-		if(input.charAt(1) == answer.charAt(1)) {
+		if (input.charAt(1) == answer.charAt(1)) {
 			ret[0]++;
 		}
-		if(input.charAt(1)== answer.charAt(2)) {
+		if (input.charAt(1) == answer.charAt(2)) {
 			ret[1]++;
 		}
-		if(input.charAt(2) == answer.charAt(0)) {
+		if (input.charAt(2) == answer.charAt(0)) {
 			ret[1]++;
 		}
-		if(input.charAt(2) == answer.charAt(1)) {
+		if (input.charAt(2) == answer.charAt(1)) {
 			ret[1]++;
 		}
-		if(input.charAt(2)== answer.charAt(2)) {
+		if (input.charAt(2) == answer.charAt(2)) {
 			ret[0]++;
 		}
 		return ret;
 	}
 
-
-
 	private static void guide() {
 		System.out.print("숫자를 입력해주세요 : ");
 	}
 
-	private static String generate(String answer) {
+	private static String generateAnswer(String answer) {
 		if (answer.length() == 3 && answer.charAt(0) != answer.charAt(1) && answer.charAt(1) != answer.charAt(2) && answer.charAt(0) != answer.charAt(2)) {
 			return answer;
 		}
@@ -112,7 +114,7 @@ public class Main {
 			if (answer.length() == 3 && answer.charAt(0) != answer.charAt(1) && answer.charAt(1) != answer.charAt(2) && answer.charAt(0) != answer.charAt(2)) {
 				break;
 			}
-		}		
+		}
 		return answer;
 	}
 
