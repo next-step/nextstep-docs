fb7baa4c3dd09c211e151aab270ebc26f334af6e
yoonjaej3 <yoonjaej3@gmail.com>
2022-04-16 18:08:33 +0900

    ParamterizedTest

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,11 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -25,6 +30,20 @@ public class SetTest {
         int actual=numbers.size();
         System.out.println("Set 크기 Test");
         assertEquals(3,actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints={1,2,3})
+    void contains(int element) {
+        System.out.println(numbers);
+        assertThat(numbers.contains(element)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(int input,String expected) {
+        String actual=Boolean.toString(numbers.contains(input));
+        assertEquals(expected,actual);
     }
 
 }
